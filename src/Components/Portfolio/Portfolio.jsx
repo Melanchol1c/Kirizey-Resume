cc026dcd2d22a340e730f490ab2c0d05cb42fc1c
Spark1er <ricksanford498@gmail.com>
2019-01-05 00:28:46 +0700

    update tools

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -24,7 +24,7 @@ export default class Portfolio extends Component {
         id: 4,
         name: "Oxbridge",
         description:
-          'Web dashboard for teachers of the school of English "Oxbridge". Storing data about students, their parents, the schedule and module of the audition, on the basis of which the group is determined by the new student.',
+          'Web dashboard for teachers of the school of English "Oxbridge". Storing data about students, their parents, the schedule and module of the audition, on the basis of which the group is determined by the new student. Test data: admin:admin.',
         tools: [
           "Django",
           "Django-rest-framework",
@@ -33,7 +33,7 @@ export default class Portfolio extends Component {
           "JQuery"
         ],
         gitRepo: "",
-        prodUrl: "http://194.177.23.60",
+        prodUrl: "https://oxbridge-demp.herokuapp.com/",
         img: OxbridgeImg
       },
       {
@@ -41,7 +41,13 @@ export default class Portfolio extends Component {
         name: "Kirizey Blog",
         description:
           "Simple news blog for course work. CRUD, like-dislike and commenting.",
-        tools: ["React", "Django", "Django-rest-framework", "Ant Design"],
+        tools: [
+          "React",
+          "Django",
+          "Django-rest-framework",
+          "Ant Design",
+          "Saas"
+        ],
         gitRepo: "https://github.com/Melanchol1c/Kirizey-Blog",
         prodUrl: "https://kirizey-blog.herokuapp.com",
         img: KirizeyBlogImg
@@ -51,7 +57,7 @@ export default class Portfolio extends Component {
         name: "Kirizey Bulto",
         description:
           "Simple To Do and budget manager. Project for training React framework.",
-        tools: ["React", "Materialize"],
+        tools: ["React", "Materialize", "Saas"],
         gitRepo: "https://github.com/Melanchol1c/kirizey-bulto",
         prodUrl: "https://kirizey-bulto.firebaseapp.com/",
         img: KirizeyBultoImg
