49c2fcfc527ff0e8008d41a5825ac893891f6f7e
Spark1er <ricksanford498@gmail.com>
2019-01-07 20:39:03 +0700

    replace projects data in App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,74 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import About from "./Components/About/About";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import KirizeyPlayImg from "../src/assets/kirizey-play-img.png";
+import KirizeyBultoImg from "../src/assets/kirizey-bulto-img.png";
+import KirizeyBlogImg from "../src/assets/kirizey-blog-img.png";
+import OxbridgeImg from "../src/assets/oxbridge-img.png";
 
 class App extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "Kirizey Play",
+        description: "Game shop using SPA pattern on Angular 4.",
+        tools: ["Angular", "Bootstrap", "Firebase"],
+        gitRepo: "https://github.com/Melanchol1c/Kirizey-Play",
+        prodUrl: "https://kirizey-shop.firebaseapp.com/",
+        img: KirizeyPlayImg
+      },
+      {
+        id: 4,
+        name: "Oxbridge",
+        description:
+          'Web dashboard for teachers of the school of English "Oxbridge". Storing data about students, their parents, the schedule and module of the audition, on the basis of which the group is determined by the new student. Test data: admin:admin.',
+        tools: [
+          "Django",
+          "Django-rest-framework",
+          "React",
+          "Bootstrap",
+          "JQuery"
+        ],
+        gitRepo: "",
+        prodUrl: "https://oxbridge-demp.herokuapp.com/",
+        img: OxbridgeImg
+      },
+      {
+        id: 2,
+        name: "Kirizey Blog",
+        description:
+          "Simple news blog for course work. CRUD, like-dislike and commenting.",
+        tools: [
+          "React",
+          "Django",
+          "Django-rest-framework",
+          "Ant Design",
+          "Saas"
+        ],
+        gitRepo: "https://github.com/Melanchol1c/Kirizey-Blog",
+        prodUrl: "https://kirizey-blog.herokuapp.com",
+        img: KirizeyBlogImg
+      },
+      {
+        id: 3,
+        name: "Kirizey Bulto",
+        description:
+          "Simple To Do and budget manager. Project for training React framework.",
+        tools: ["React", "Materialize", "Saas"],
+        gitRepo: "https://github.com/Melanchol1c/kirizey-bulto",
+        prodUrl: "https://kirizey-bulto.firebaseapp.com/",
+        img: KirizeyBultoImg
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
         <Header />
         <About />
-        <Portfolio />
+        <Portfolio data={this.state.projects} />
       </div>
     );
   }
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,72 +1,16 @@
 import React, { Component } from "react";
 import "./Portfolio.scss";
-import KirizeyPlayImg from "../../assets/kirizey-play-img.png";
-import KirizeyBultoImg from "../../assets/kirizey-bulto-img.png";
-import KirizeyBlogImg from "../../assets/kirizey-blog-img.png";
-import OxbridgeImg from "../../assets/oxbridge-img.png";
 import LazyLoad from "react-lazyload";
 
 export default class Portfolio extends Component {
   state = {
     activeParam: "All",
-    params: ["All", "React", "Angular", "Django"],
-    projects: [
-      {
-        id: 1,
-        name: "Kirizey Play",
-        description: "Game shop using SPA pattern on Angular 4.",
-        tools: ["Angular", "Bootstrap", "Firebase"],
-        gitRepo: "https://github.com/Melanchol1c/Kirizey-Play",
-        prodUrl: "https://kirizey-shop.firebaseapp.com/",
-        img: KirizeyPlayImg
-      },
-      {
-        id: 4,
-        name: "Oxbridge",
-        description:
-          'Web dashboard for teachers of the school of English "Oxbridge". Storing data about students, their parents, the schedule and module of the audition, on the basis of which the group is determined by the new student. Test data: admin:admin.',
-        tools: [
-          "Django",
-          "Django-rest-framework",
-          "React",
-          "Bootstrap",
-          "JQuery"
-        ],
-        gitRepo: "",
-        prodUrl: "https://oxbridge-demp.herokuapp.com/",
-        img: OxbridgeImg
-      },
-      {
-        id: 2,
-        name: "Kirizey Blog",
-        description:
-          "Simple news blog for course work. CRUD, like-dislike and commenting.",
-        tools: [
-          "React",
-          "Django",
-          "Django-rest-framework",
-          "Ant Design",
-          "Saas"
-        ],
-        gitRepo: "https://github.com/Melanchol1c/Kirizey-Blog",
-        prodUrl: "https://kirizey-blog.herokuapp.com",
-        img: KirizeyBlogImg
-      },
-      {
-        id: 3,
-        name: "Kirizey Bulto",
-        description:
-          "Simple To Do and budget manager. Project for training React framework.",
-        tools: ["React", "Materialize", "Saas"],
-        gitRepo: "https://github.com/Melanchol1c/kirizey-bulto",
-        prodUrl: "https://kirizey-bulto.firebaseapp.com/",
-        img: KirizeyBultoImg
-      }
-    ]
+    params: ["All", "React", "Angular", "Django"]
   };
 
   render() {
-    let { projects, params, activeParam } = this.state;
+    let { params, activeParam } = this.state;
+    let projects = this.props.data;
 
     projects =
       activeParam === "All"
